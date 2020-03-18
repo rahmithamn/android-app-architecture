f03b5349c3bc27232125ed3376bb2ae20fdc519a
rahmithamn <rahmithasaid@gmail.com>
2020-03-18 17:27:49 +0700

    add ViewModel

--- app/src/main/java/org/rmn/sleeptracker/sleeptracker/SleepTrackerFragment.kt
+++ app/src/main/java/org/rmn/sleeptracker/sleeptracker/SleepTrackerFragment.kt
@@ -7,8 +7,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProviders
 
 import org.rmn.sleeptracker.R
+import org.rmn.sleeptracker.database.SleepDatabase
 import org.rmn.sleeptracker.databinding.FragmentSleepTrackerBinding
 
 /**
@@ -23,6 +25,16 @@ class SleepTrackerFragment : Fragment() {
         val binding: FragmentSleepTrackerBinding = DataBindingUtil.inflate(
             inflater, R.layout.fragment_sleep_tracker, container, false)
 
+        val application = requireNotNull(this.activity).application
+        val dataSource = SleepDatabase.getInstance(application).sleepDatabaseDao
+        val viewModelFactory = SleepTrackerViewModelFactory(dataSource, application)
+
+        val sleepTrackerViewModel =
+            ViewModelProviders.of(
+                this, viewModelFactory).get(SleepTrackerViewModel::class.java)
+
+        binding.setLifecycleOwner(this)
+        binding.sleepTrackerViewModel = sleepTrackerViewModel
         return binding.root
     }
 
