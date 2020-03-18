f9f28f5a961b3059c75ae84bcff538e79a72727d
rahmithamn <rahmithasaid@gmail.com>
2020-03-18 18:18:12 +0700

    Coroutines

--- app/src/main/java/org/rmn/sleeptracker/Util.kt
+++ app/src/main/java/org/rmn/sleeptracker/Util.kt
@@ -1,6 +1,13 @@
 package org.rmn.sleeptracker
 
+import android.annotation.SuppressLint
 import android.content.res.Resources
+import android.os.Build
+import android.text.Html
+import android.text.Spanned
+import androidx.core.text.HtmlCompat
+import org.rmn.sleeptracker.database.SleepNight
+import java.text.SimpleDateFormat
 
 fun convertNumericQualityToString(quality: Int, resources: Resources): String {
     var qualityString = resources.getString(R.string.three_ok)
@@ -13,4 +20,38 @@ fun convertNumericQualityToString(quality: Int, resources: Resources): String {
         5 -> qualityString = resources.getString(R.string.five_excellent)
     }
     return qualityString
+}
+@SuppressLint("SimpleDateFormat")
+fun convertLongToDateString(systemTime: Long): String {
+    return SimpleDateFormat("EEEE MMM-dd-yyyy' Time: 'HH:mm")
+        .format(systemTime).toString()
+}
+fun formatNights(nights: List<SleepNight>, resources: Resources): Spanned {
+    val sb = StringBuilder()
+    sb.apply {
+        append(resources.getString(R.string.title))
+       nights.forEach {
+           append("<br>")
+           append(resources.getString(R.string.start_time))
+            append("\t${convertLongToDateString(it.startTimeMilli)}<br>")
+            if (it.endTimeMilli != it.startTimeMilli) {
+                append(resources.getString(R.string.end_time))
+                append("\t${convertLongToDateString(it.endTimeMilli)}<br>")
+                append(resources.getString(R.string.quality))
+                append("\t${convertNumericQualityToString(it.sleepQuality, resources)}<br>")
+                append(resources.getString(R.string.hours_slept))
+                // Hours
+                append("\t ${it.endTimeMilli.minus(it.startTimeMilli) / 1000 / 60 / 60}:")
+                // Minutes
+                append("${it.endTimeMilli.minus(it.startTimeMilli) / 1000 / 60}:")
+                // Seconds
+                append("${it.endTimeMilli.minus(it.startTimeMilli) / 1000}<br><br>")
+            }
+        }
+    }
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+        return Html.fromHtml(sb.toString(), Html.FROM_HTML_MODE_LEGACY)
+    } else {
+        return HtmlCompat.fromHtml(sb.toString(), HtmlCompat.FROM_HTML_MODE_LEGACY)
+    }
 }
